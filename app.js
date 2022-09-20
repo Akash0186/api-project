ec2ca941acda3261d58ca4e25fa9bd09ebd480bc
Akash0186 <84200802+Akash0186@users.noreply.github.com>
2022-09-20 13:47:33 +0600

    js file added

--- app.js
+++ app.js
@@ -64,7 +64,27 @@ function mealRecipeModal(meal){
     <p class="recipe-category">${meal.strCategory}</p>
     <div class="recipe-instruct">
         <h3>Ingredients:</h3>
-        <p>${meal.strInstructions}</p>
+        <ul id="ingredient-style">
+        <li>${meal.strIngredient1}</li>
+        <li>${meal.strIngredient2}</li>
+        <li>${meal.strIngredient3}</li>
+        <li>${meal.strIngredient4}</li>
+        <li>${meal.strIngredient5}</li>
+        <li>${meal.strIngredient6}</li>
+        <li>${meal.strIngredient7}</li>
+        <li>${meal.strIngredient8}</li>
+        <li>${meal.strIngredient9}</li>
+        <li>${meal.strIngredient10}</li>
+        <li>${meal.strIngredient11}</li>
+        <li>${meal.strIngredient12}</li>
+        <li>${meal.strIngredient13}</li>
+        <li>${meal.strIngredient14}</li>
+        <li>${meal.strIngredient15}</li>
+        <li>${meal.strIngredient16}</li>
+        <li>${meal.strIngredient17}</li>
+        <li>${meal.strIngredient18}</li>
+        <li>${meal.strIngredient19}</li>
+        </ul>
     </div>
    </div>
       <div class="recipe-meal-img">
